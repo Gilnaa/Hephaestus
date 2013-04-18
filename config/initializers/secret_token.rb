ac07ef9ed0ca7124bd23a58d08bc7a7edf5521b8
Gilad Naaman <gilad.doom@gmail.com>
2013-04-18 12:28:11 +0300

    Initial Commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hephaestus::Application.config.secret_token = '2950f7c25343ce518fcf857f574a2d2538c864849da7f5fcf92f9839caf648ae91db8effd53bf7660399122a70fe22691c507d6a22641d5246cc8cc24d0c1e63'
